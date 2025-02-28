08219a197644dfe2c65f8e1c907a1153d37e443e
Trinh Nguyen Hoang Vu <trinhnguyenhoangvu306@gmail.com>
2025-02-28 21:43:25 +0700

    Create usecase login/register

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
+
+db = SQLAlchemy()
+DB_NAME = 'datauser.db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'mysecretkey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -10,4 +18,22 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not os.path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Created database!')
