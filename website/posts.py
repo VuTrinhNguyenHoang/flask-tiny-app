0a143a02b43ed962d774664c857f6cf5323f6870
Trinh Nguyen Hoang Vu <trinhnguyenhoangvu306@gmail.com>
2025-03-04 22:02:34 +0700

    Add create/edit post for User

--- website/posts.py
+++ website/posts.py
@@ -12,21 +12,61 @@ def post_detail(slug):
 @posts.route('/manage-posts')
 @login_required
 def manage_posts():
-    return render_template('manage_posts.html', user=current_user)
+    posts = Post.query.filter_by(user_id=current_user.id).order_by(Post.updated_at.desc()).all()
+    return render_template('manage_posts.html', user=current_user, posts=posts)
 
 @posts.route('/delete-posts', methods=['POST'])
 @login_required
 def delete_posts():
     post_ids = request.form.getlist('post_ids')
     if post_ids:
-        Post.query.filter(Post.id.in_(post_ids), Post.user_id == current_user.id) \
-                    .delete(synchronize_session=False)
+        Post.query.filter(Post.id.in_(post_ids), Post.user_id == current_user.id).delete(synchronize_session=False)
         db.session.commit()
         flash(f'Đã xóa {len(post_ids)} bài viết thành công', 'success')
     else:
         flash('Vui lòng chọn ít nhất một bài viết để xóa', 'warning')
-    
+
     return redirect(url_for('posts.manage_posts'))
+
+@posts.route('/create-post', methods=['GET', 'POST'])
+@login_required
+def create_post():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        if not title or not content:
+            flash('Tiêu đề và nội dung không được để trống', 'error')
+            return redirect(url_for('posts.create_post'))
+
+        new_post = Post(title=title, content=content, user_id=current_user.id)
+        db.session.add(new_post)
+        db.session.commit()
+        flash('Bài viết đã được tạo thành công!', 'success')
+        return redirect(url_for('posts.manage_posts'))
+
+    return render_template('create_post.html', user=current_user)
+
+@posts.route('/edit-post/<string:slug>', methods=['GET', 'POST'])
+@login_required
+def edit_post(slug):
+    post = Post.query.filter_by(slug=slug, user_id=current_user.id).first_or_404()
+
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        if not title or not content:
+            flash('Tiêu đề và nội dung không được để trống', 'error')
+            return redirect(url_for('posts.edit_post', slug=slug))
+
+        post.title = title
+        post.content = content
+        db.session.commit()
+        flash('Bài viết đã được cập nhật!', 'success')
+        return redirect(url_for('posts.manage_posts'))
+
+    return render_template('edit_post.html', post=post, user=current_user)
 
 @posts.route('/api/posts')
 def get_posts():
@@ -35,19 +75,19 @@ def get_posts():
     get_all = request.args.get('all', 'false') == 'true'
 
     if get_all:
-        pagination = Post.query.order_by(Post.updated_at.desc()) \
-                                .paginate(page=page, per_page=per_page, error_out=False)
+        pagination = Post.query.order_by(Post.updated_at.desc()).paginate(page=page, per_page=per_page, error_out=False)
     else:
-        pagination = Post.query.filter_by(user_id=current_user.id) \
-                                .paginate(page=page, per_page=per_page, error_out=False)
+        pagination = Post.query.filter_by(user_id=current_user.id).order_by(Post.updated_at.desc()).paginate(page=page, per_page=per_page, error_out=False)
 
     posts_data = [
-        {'id': post.id,
-         'title': post.title,
-         'updated_at': post.updated_at.strftime('%d/%m/%Y'),
-         'content': post.content,
-         'slug': post.slug,
-         'author': post.author.username}
+        {
+            'id': post.id,
+            'title': post.title,
+            'updated_at': post.updated_at.strftime('%d/%m/%Y'),
+            'content': post.content[:100],
+            'slug': post.slug,
+            'author': post.author.username
+        }
         for post in pagination.items
     ]
 
